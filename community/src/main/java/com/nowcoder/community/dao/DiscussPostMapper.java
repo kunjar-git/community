906e6111bb93f0c0f349063f2ab62b7446d0c2a4
kunjar-git <2250846475@qq.com>
2023-09-22 16:15:52 +0800

    第二次提交

--- community/src/main/java/com/nowcoder/community/dao/DiscussPostMapper.java
+++ community/src/main/java/com/nowcoder/community/dao/DiscussPostMapper.java
@@ -8,7 +8,8 @@ import java.util.List;
 
 @Mapper
 public interface DiscussPostMapper {
-    // userId为了扩展“我的贴子数量”而定义，动态SQL
+    // userId为了扩展“我的
+    // 贴子数量”而定义，动态SQL
     List<DiscussPost> selectDiscussPosts(int userId, int offset, int limit);
     // @Param：给参数起别名
     // 如果需要动态拼条件且只有一个参数，那么必须用的
